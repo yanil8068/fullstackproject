c363f5e76feabc9594197214ae946a67bf8a228f
yanil8068 <yanil8068@gmail.com>
2024-05-09 14:18:38 +0530

    connect to local mongodb

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/project");
+}
+main().then(() => {
+  console.log("db connected");
+});
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
   res.send("home route working");
